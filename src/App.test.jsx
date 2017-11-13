ad9e3a8538ab6be213469205305758aa1fb4af7e
Allison Rodriguez <allisonhope10@gmail.com>
2017-11-13 16:16:55 +0100

    flesh out testing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+Enzyme.configure({ adapter: new Adapter() });
+
+import SearchApp from './App';
+
+describe('SearchApp', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<SearchApp />, div);
+  });
+
+  it('should return search results', () => {
+  const output = shallow(
+    <SearchApp />
+  );
+  const searchBtn = shallow(
+    <button></button>
+  );
+  
+  expect(output.state().searchResults).toEqual([]);
+  searchBtn.simulate('click');
+  expect(output.state().searchResults).toEqual([]);
+});
 });
